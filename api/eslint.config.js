249b51113690e0098a160d3e4ed87387bf1dcfbb
Huseen <136820327+hmunye@users.noreply.github.com>
2024-09-26 09:07:57 -0400

    added eslint and prettier config for linting and formatting

---
+++ api/eslint.config.js
@@ -0,0 +1,27 @@
+import globals from 'globals';
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default [
+  { languageOptions: { globals: globals.node } },
+  js.configs.recommended,
+  eslintConfigPrettier,
+  {
+    files: ['**/*.js'],
+    ignores: ['/node_modules'],
+    rules: {
+      eqeqeq: 'warn', // Enforce strict equality
+      curly: 'error', // Require curly braces for control statements
+      'no-unused-vars': 'warn', // Warn on unused variables
+
+      'no-var': 'error', // Disallow var, enforce let/const
+      'prefer-const': 'warn', // Prefer const when variables are not reassigned
+      'no-undef': 'error', // Disallow the use of undeclared variables
+
+      'callback-return': 'warn', // Warn if callback is not returned
+
+      'no-useless-escape': 'warn', // Warn for unnecessary escape sequences
+      'no-duplicate-imports': 'error', // Disallow duplicate import statements
+    },
+  },
+];
--- api/src/server.js
+++ api/src/server.js
@@ -4,7 +4,6 @@ import { healthCheckRoutes as healthCheck } from "./routes/healthCheck.js";
 export default function build(config) {
   const app = express();
 
-  // Middleware
   app.use(express.json());
 
   app.use("/health-check", healthCheck(config));
