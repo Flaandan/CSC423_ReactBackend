2f47ab656ca6ca16d38ff7b077c77a977a7ab33d
Flaandan <charr10@brockport.edu>
2024-10-06 16:08:16 -0400

    Changed frontend files see description for specifics

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,21 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import Login from './Login.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import Login from './Login.jsx';
+import AdminDash from './adminDash.jsx';
+import TeacherDash from './teacherDash.jsx';
+import StudentDash from './studentDash.jsx';
+import Root from "./routes/root";
 import {
-  createBrowserRouter,
-  RouterProvider,
+    createBrowserRouter,
+    RouterProvider,
 } from "react-router-dom";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Login />,
-  },
+    {
+        path: "/",
+        element: <Login />,
+    },
+    {
+        path: "/AdminDash",
+        element: <AdminDash />,
+    },
+    {
+        path: "/studentDash",
+        element: <StudentDash />,
+    },
+    {
+        path: "/teacherDash",
+        element: <TeacherDash />,
+    }
 ]);
 
-
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>,
 )
