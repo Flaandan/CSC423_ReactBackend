d6558077df4a707a54699fe074510da22771425e
LiteralGoblin <a.s.thomas94@gmail.com>
2024-10-07 14:10:53 -0400

    Testing

--- client/src/studentDash.jsx
+++ client/src/studentDash.jsx
@@ -1,13 +1,44 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Popup } from 'reactjs-popup';
 import './webPage.css';
 
 
+
 const studentDash = () => {
+    const navigate = useNavigate();
+    const handleLogout = () => {
+        navigate('/');
+    };
+    const handleChangePass = () => {}
+    /* I want to add all the functional buttons to the side column, main page
+     * should be used for displaying the interaction information*/
     return (
-        <div>
-            <h1>Student Dashboard</h1>
-            <p>Welcome to the Student Dashboard!</p>
-        </div>
+        <div className="student-dashboard">
+            <div className="left-column">
+                <h1>Student Dashboard</h1>
+                {/*Place Holders*/}
+                <a href="#classes">Regiester for Classes</a>
+                
+
+                <button onClick={handleLogout} className="logout-button">
+                    Logout
+                </button>
+
+                {/*
+                    <div>
+                    <button onClick={() => setOpen(true)}>Open Popup</button>
+                    <Popup open={open} onClose={() => setOpen(false)}>
+                        <div>
+                        <h2>Popup Title</h2>
+                        <p>Popup content</p>
+                        </div>
+                    </Popup>
+                    </div>
+                */}
+            </div>
+            </div>
+   
     );
 };
 
